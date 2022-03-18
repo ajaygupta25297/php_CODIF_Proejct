a40675df0e1d42602068b05ce95c917702451e49
ajaygupta25297 <ajaygupta25297@gmail.com>
2022-03-18 23:38:34 +0530

    third commit

--- welcome.php
+++ welcome.php
@@ -23,6 +23,14 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
   <body>
     <?php require 'partials/_nav.php' ?>
     Welcome - <?php echo $_SESSION['username']?>
+    <div class="container">
+    <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Well done!</h4>
+        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+        <hr>
+        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+    </div>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
